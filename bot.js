570b2bfb77b757c9ccc869a2a72bb0b121424980
m3roof1441 <44141768+m3roof1441@users.noreply.github.com>
2018-10-29 04:47:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,34 +17,7 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => {
-    var prefix = "$"
-    if (message.content === prefix + 'create') {
-        if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-                if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
-         message.guild.createChannel('✵-「news', 'text')
-         message.guild.createChannel('✵-「chat', 'text')
-         message.guild.createChannel('✵-「commands', 'text')
-         message.guild.createChannel('✵-「pic', 'text')
-         message.guild.createChannel('✵-「gaming', 'text')
-         message.guild.createChannel('✵-「vote', 'text')
-         message.guild.createChannel('✵-「giveaway', 'text')
-         message.guild.createChannel('✵-「Quran_1', 'voice')
-         message.guild.createChannel('✵-「Quran_2', 'voice')
-         message.guild.createChannel('✵-「Quran_3', 'voice')
-         message.guild.createChannel('✵-「Music_1', 'voice')
-         message.guild.createChannel('✵-「Music_2', 'voice')
-         message.guild.createChannel('✵-「Music_3', 'voice')
-         message.guild.createChannel('✵-「Minecraft', 'voice')
-         message.guild.createChannel('✵-「Fortnite', 'voice')
-         message.guild.createChannel('✵-「Blackops', 'voice')
-         message.guild.createChannel('✵-「Race', 'voice')
-         message.guild.createChannel('✵-「Life For Speed', 'voice')
-         message.guild.createChannel('✵-「MTA', 'voice')
-         message.guild.createChannel('✵-「GTA V', 'voice')
-    message.channel.sendMessage('**تـم انشاء الرومات بنجاح**')
-    }
-    });
+
 
 
 client.login('BOT_TOKEN');
